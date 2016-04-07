ec70f63000e904c42e2d42c46e70922c5b4b293f
Christoph Rieger <christoph.rieger@wi.uni-muenster.de>
2016-04-07 15:14:09 +0200

    Still working after cleanup

--- src/de/wwu/md2dot0/diagram/ProcessElementEditPart.java
+++ src/de/wwu/md2dot0/diagram/ProcessElementEditPart.java
@@ -44,371 +44,199 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 
 import md2dot0.ProcessElement;
 
-public class ProcessElementEditPart extends AbstractNotSelectableShapeNodeEditPart implements IStyleEditPart { //AbstractNotSelectableShapeNodeEditPart
+public class ProcessElementEditPart extends AbstractNotSelectableShapeNodeEditPart implements IStyleEditPart {
 
-	/*******************************************************************************
-	 * Copyright (c) 2007, 2008 THALES GLOBAL SERVICES.
-	 * All rights reserved. This program and the accompanying materials
-	 * are made available under the terms of the Eclipse Public License v1.0
-	 * which accompanies this distribution, and is available at
-	 * http://www.eclipse.org/legal/epl-v10.html
-	 *
-	 * Contributors:
-	 *    Obeo - initial API and implementation
-	 *******************************************************************************/
-	
-	    /**
-	     * @not-generated : prevent drag of elements
-	     */
-	    @Override
-	    public DragTracker getDragTracker(Request request) {
-	        return getParent().getDragTracker(request);
-	    }
-
-	    /**
-	     * @see org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeEditPart#refreshVisuals()
-	     * @not-generated
-	     */
-	    @Override
-	    protected void refreshVisuals() {
-	        super.refreshVisuals();
-	        GraphicalEditPart part = ((GraphicalEditPart) getParent());
-	        
-	        EObject dde = this.resolveSemanticElement();
-	        if (dde instanceof Square) {
-	            Square square = (Square) dde;
-	            int borderSize = 0;
-	            if (square.getBorderSize() != null) {
-	                borderSize = square.getBorderSize().intValue();
-	            }
-//	            this.getPrimaryShape().setLineWidth(borderSize);
-	            DiagramNodeEditPartOperation.refreshFigure(this);
-	            DiagramElementEditPartOperation.refreshLabelAlignment(((GraphicalEditPart) getParent()).getContentPane(), square);
-	        }
-	    }
-
-	    /**
-	     * @not-generated
-	     * @see org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart#refreshBackgroundColor()
-	     */
-	    @Override
-	    protected void refreshBackgroundColor() {
-	        if (getMetamodelType().isInstance(resolveSemanticElement())) {
-	            Square square = (Square) this.resolveSemanticElement();
-	            if (square.getColor() != null) {
-	                this.getPrimaryShape().setBackgroundColor(VisualBindingManager.getDefault().getColorFromRGBValues(square.getColor()));
-	            }
-	        }
-	    }
-
-	    /**
-	     * @not-generated
-	     * @see org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart#refreshForegroundColor()
-	     */
-	    @Override
-	    protected void refreshForegroundColor() {
-	        if (getMetamodelType().isInstance(resolveSemanticElement())) {
-	            Square square = (Square) this.resolveSemanticElement();
-	            if (square.getBorderColor() != null) {
-	                this.getPrimaryShape().setForegroundColor(VisualBindingManager.getDefault().getColorFromRGBValues(square.getBorderColor()));
-	            }
-	        }
-	    }
-
-	    /**
-	     * @was-generated
-	     */
-	    //public static final int VISUAL_ID = 3003;
-
-	    /**
-	     * @was-generated
-	     */
-	    protected IFigure contentPane;
-
-	    /**
-	     * @was-generated
-	     */
-	    protected ProcessElementFigure2 primaryShape;
-
-	    /**
-	     * @was-generated
-	     */
-	    public ProcessElementEditPart(View view) {
-	        super(view);
-	    }
-
-	    /**
-	     * @not-generated
-	     */
-	    @Override
-	    protected void createDefaultEditPolicies() {
-	        // Do nothing.
-	    }
-
-	    /**
-	     * @not-generated
-	     */
-	    protected LayoutEditPolicy createLayoutEditPolicy() {
-	        return new FixedLayoutEditPolicy();
-	    }
-
-	    /**
-	     * @not-generated
-	     */
-	    protected IFigure createNodeShape() {
-//	        SquareFigure square = new SquareFigure();
-//	        EditPart parent = this.getParent();
-//	        if (parent instanceof IDiagramBorderNodeEditPart) {
-//	            DiagramBorderNodeEditPartOperation.updateTransparencyMode((IDiagramBorderNodeEditPart) parent, square);
-//	        }
-	    	ProcessElementFigure2 square = new ProcessElementFigure2((CustomStyle) this.resolveSemanticElement());
-	        return primaryShape = square;
-	    }
-
-	    /**
-	     * @was-generated
-	     */
-	    public ProcessElementFigure2 getPrimaryShape() {
-	        return (ProcessElementFigure2) primaryShape;
-	    }
-
-	    /**
-	     * @not-generated
-	     */
-	    protected NodeFigure createNodePlate() {
-	        return new AirStyleDefaultSizeNodeFigure(getMapMode().DPtoLP(40), getMapMode().DPtoLP(40));
-	    }
-
-	    /**
-	     * @was-generated
-	     */
-	    @Override
-	    public EditPolicy getPrimaryDragEditPolicy() {
-	        EditPolicy result = super.getPrimaryDragEditPolicy();
-	        if (result instanceof ResizableEditPolicy) {
-	            ResizableEditPolicy ep = (ResizableEditPolicy) result;
-	            ep.setResizeDirections(PositionConstants.NONE);
-	        }
-	        return result;
-	    }
-
-	    /**
-	     * Creates figure for this edit part.
-	     * 
-	     * Body of this method does not depend on settings in generation model so
-	     * you may safely remove <i>generated</i> tag and modify it.
-	     * 
-	     * @was-generated
-	     */
-	    @Override
-	    protected NodeFigure createNodeFigure() {
-	        NodeFigure figure = createNodePlate();
-//	        figure.setLayoutManager(new StackLayout());
-	        figure.setLayoutManager(new XYLayout());
-	        IFigure shape = createNodeShape();
-	        figure.add(shape);
-	        contentPane = setupContentPane(shape);
-	        return figure;
-	    }
-
-	    /**
-	     * Default implementation treats passed figure as content pane. Respects
-	     * layout one may have set for generated figure.
-	     * 
-	     * @param nodeShape
-	     *            instance of generated figure class
-	     * @was-generated
-	     */
-	    protected IFigure setupContentPane(IFigure nodeShape) {
-	        return nodeShape; // use nodeShape itself as contentPane
-	    }
-
-	    /**
-	     * @was-generated
-	     */
-	    @Override
-	    public IFigure getContentPane() {
-	        if (contentPane != null) {
-	            return contentPane;
-	        }
-	        return super.getContentPane();
-	    }
-
-	    /**
-	     * @not-generated
-	     */
-//	    public static class SquareFigure extends AbstractTransparentRectangle {
-//
-//	        /**
-//	         * @was-generated
-//	         */
-//	        public SquareFigure() {
-//	            this.setLineWidth(3);
-//	        }
-//	    }
-
-	    protected Class<?> getMetamodelType() {
-	        return Square.class;
-	    }
-
+	@Override
+	public DragTracker getDragTracker(Request request) {
+		return getParent().getDragTracker(request);
 	}
 
-//	/**
-//	 * the content pane.
-//	 */
-//	protected IFigure contentPane;
-//
-//	/**
-//	 * the primary shape.
-//	 */
-//	protected ProcessElementFigure arrowShape;
-//
-//	/**
-//	 * Create a new {@link ChangingImageEditPart}.
-//	 *
-//	 * @param view
-//	 *            the view.
-//	 */
-//	public ProcessElementEditPart(View view) {
-//		super(view);
-//	}
-//
-//	public DragTracker getDragTracker(Request request) {
-//		return getParent().getDragTracker(request);
-//	}
-//
-//	protected NodeFigure createNodeFigure() {
-//		NodeFigure figure = createNodePlate();
-//		figure.setLayoutManager(new XYLayout());
-//		IFigure shape = createArrowShape();
-//		figure.add(shape);
-//
-//		contentPane = setupContentPane(shape);
-//		return figure;
-//	}
-//
-//	private NodeFigure createNodePlate() {
-//		DefaultSizeNodeFigure result = new AirStyleDefaultSizeNodeFigure(20, 10);
-//		return result;
-//	}
-//
-//	protected IFigure createArrowShape() {
-//		if (arrowShape == null) {
-//			arrowShape = new ProcessElementFigure((CustomStyle) this.resolveSemanticElement());
-//		}
-//		return arrowShape;
-//	}
-//
-//	/**
-//	 * Return the instance role figure.
-//	 *
-//	 * @return the instance role figure.
-//	 */
-//	public IFigure getPrimaryShape() {
-//		return arrowShape;
-//	}
-//
-//	/**
-//	 * Default implementation treats passed figure as content pane. Respects
-//	 * layout one may have set for generated figure.
-//	 *
-//	 * @param nodeShape instance of generated figure class
-//	 * @return the figure
-//	 */
-//	protected IFigure setupContentPane(IFigure nodeShape) {
-//		return nodeShape; // use nodeShape itself as contentPane
-//	}
-//
-//	public IFigure getContentPane() {
-//		if (contentPane != null) {
-//			return contentPane;
-//		}
-//		return super.getContentPane();
-//	}
-//
-//	protected void refreshVisuals() {
-//		CustomStyle customStyle = (CustomStyle) this.resolveSemanticElement();
-//		if (arrowShape != null && customStyle.eContainer() instanceof DNode) {
-//			DNode node = (DNode) customStyle.eContainer();
-//			
-//			// Update description
-//			arrowShape.setProcessElementDescription(node.getName());
-//			
-//			if(node.getTarget() instanceof ProcessElement && node.getTarget() != null){
-//				ProcessElement modelElement = (ProcessElement) node.getTarget();
-//				
-//				// Update process element subtype
-//				arrowShape.setProcessElementType(modelElement.getClass().getSimpleName());
-//				
-//				// Update data type
-//				String dataTypeName = modelElement.getDataType() != null ? modelElement.getDataType().toString() : "X";
-//				arrowShape.setProcessElementDataType(dataTypeName); // Todo infer from service class?
-//			}
-//		}
-//	}
-//	
-//	
-//		
-//	
-//	
-////	@Override
-////	protected void createDefaultEditPolicies() {
-////		// empty
-////		installEditPolicy(EditPolicy.DIRECT_EDIT_ROLE, new ProcessElementEditPolicy());
-////    }
-//	
-////	protected void createDefaultEditPolicies(){
-////		//super.createEditPolicies();
-//////		installEditPolicy(EditPolicy.DIRECT_EDIT_ROLE, new ProcessElementEditPolicy());
-////	}
-//	
-////	public void performRequest(Request request){
-////	if (request.getType() == RequestConstants.REQ_DIRECT_EDIT) {
-////	if(manager == null)
-////	manager = new LogicLabelEditManager(this,
-////	TextCellEditor.class, new
-////	LabelCellEditorLocator((Label)getFigure()));
-////	manager.show();
-////	}
-////	}
-//	
-////	@Override
-////	protected void performDirectEditRequest(Request request) {
-////		EditPart editPart = this;
-////		if (request instanceof DirectEditRequest){
-////			Point p = new Point(((DirectEditRequest)request).getLocation());
-////			getFigure().translateToRelative(p);
-////			IFigure fig = getFigure().findFigureAt(p);
-////			editPart =(EditPart) getViewer().getVisualPartMap().get(fig);
-////		}
-////		if (editPart == this) {
-////			try {
-////				editPart = (EditPart) getEditingDomain().runExclusive(
-////					new RunnableWithResult.Impl() {
-////
-////						public void run() {
-////							setResult(getPrimaryChildEditPart());
-////						}
-////					});
-////			} catch (InterruptedException e) {
-////				Trace.catching(DiagramUIPlugin.getInstance(),
-////					DiagramUIDebugOptions.EXCEPTIONS_CATCHING, getClass(),
-////					"performDirectEditRequest", e); //$NON-NLS-1$
-////				Log.error(DiagramUIPlugin.getInstance(),
-////					DiagramUIStatusCodes.IGNORED_EXCEPTION_WARNING,
-////					"performDirectEditRequest", e); //$NON-NLS-1$
-////			}
-////			if (editPart != null){
-////				editPart.performRequest(request);
-////			}
-////		}
-////	}
-////	
-////	public EditPart getTargetEditPart(Request request) {
-////
-////        if (RequestConstants.REQ_DIRECT_EDIT == request.getType()){
-////        	 return super.getTargetEditPart(request);
-////        }
-////        return super.getTargetEditPart(request);
-////	}
-//}
+	/**
+	 * @see org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeEditPart#refreshVisuals()
+	 * @not-generated
+	 */
+	@Override
+	protected void refreshVisuals() {
+		super.refreshVisuals();
+		CustomStyle customStyle = (CustomStyle) this.resolveSemanticElement();
+		if (getPrimaryShape() != null && customStyle.eContainer() instanceof DNode) {
+			DNode node = (DNode) customStyle.eContainer();
+
+			if (node.getTarget() instanceof ProcessElement && node.getTarget() != null) {
+				ProcessElement modelElement = (ProcessElement) node.getTarget();
+
+				// Update process element subtype
+				getPrimaryShape().setProcessElementType(modelElement.getClass().getSimpleName());
+
+				// Update data type
+				String dataTypeName = modelElement.getDataType() != null ? modelElement.getDataType().toString() : "X";
+				getPrimaryShape().setProcessElementDataType(dataTypeName); // Todo infer from service class?
+			}
+		}
+
+		// GraphicalEditPart part = ((GraphicalEditPart) getParent());
+		//
+		// EObject dde = this.resolveSemanticElement();
+		// if (dde instanceof Square) {
+		// Square square = (Square) dde;
+		// int borderSize = 0;
+		// if (square.getBorderSize() != null) {
+		// borderSize = square.getBorderSize().intValue();
+		// }
+		// DiagramNodeEditPartOperation.refreshFigure(this);
+		// DiagramElementEditPartOperation.refreshLabelAlignment(((GraphicalEditPart)
+		// getParent()).getContentPane(), square);
+		// }
+	}
+
+	/**
+	 * @not-generated
+	 * @see org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart#refreshBackgroundColor()
+	 */
+	@Override
+	protected void refreshBackgroundColor() {
+		if (getMetamodelType().isInstance(resolveSemanticElement())) {
+			Square square = (Square) this.resolveSemanticElement();
+			if (square.getColor() != null) {
+				this.getPrimaryShape()
+						.setBackgroundColor(VisualBindingManager.getDefault().getColorFromRGBValues(square.getColor()));
+			}
+		}
+	}
+
+	/**
+	 * @not-generated
+	 * @see org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart#refreshForegroundColor()
+	 */
+	@Override
+	protected void refreshForegroundColor() {
+		if (getMetamodelType().isInstance(resolveSemanticElement())) {
+			Square square = (Square) this.resolveSemanticElement();
+			if (square.getBorderColor() != null) {
+				this.getPrimaryShape().setForegroundColor(
+						VisualBindingManager.getDefault().getColorFromRGBValues(square.getBorderColor()));
+			}
+		}
+	}
+
+	// public static final int VISUAL_ID = 3003;
+
+	/**
+	 * @was-generated
+	 */
+	protected IFigure contentPane;
+
+	/**
+	 * @was-generated
+	 */
+	protected ProcessElementFigure primaryShape;
+
+	/**
+	 * @was-generated
+	 */
+	public ProcessElementEditPart(View view) {
+		super(view);
+	}
+
+	/**
+	 * @not-generated
+	 */
+	@Override
+	protected void createDefaultEditPolicies() {
+		// Do nothing.
+	}
+
+	/**
+	 * @not-generated
+	 */
+	protected LayoutEditPolicy createLayoutEditPolicy() {
+		return new FixedLayoutEditPolicy();
+	}
+
+	/**
+	 * @not-generated
+	 */
+	protected IFigure createNodeShape() {
+		ProcessElementFigure arrowShape = new ProcessElementFigure((CustomStyle) this.resolveSemanticElement());
+
+		// EditPart parent = this.getParent();
+		// if (parent instanceof IDiagramBorderNodeEditPart) {
+		// DiagramBorderNodeEditPartOperation.updateTransparencyMode((IDiagramBorderNodeEditPart)
+		// parent, square);
+		// }
+
+		return primaryShape = arrowShape;
+	}
+
+	/**
+	 * @was-generated
+	 */
+	public ProcessElementFigure getPrimaryShape() {
+		return (ProcessElementFigure) primaryShape;
+	}
+
+	/**
+	 * @not-generated
+	 */
+	protected NodeFigure createNodePlate() {
+		return new AirStyleDefaultSizeNodeFigure(getMapMode().DPtoLP(40), getMapMode().DPtoLP(40));
+	}
+
+	/**
+	 * @was-generated
+	 */
+	@Override
+	public EditPolicy getPrimaryDragEditPolicy() {
+		EditPolicy result = super.getPrimaryDragEditPolicy();
+		if (result instanceof ResizableEditPolicy) {
+			ResizableEditPolicy ep = (ResizableEditPolicy) result;
+			ep.setResizeDirections(PositionConstants.NONE);
+		}
+		return result;
+	}
+
+	/**
+	 * Creates figure for this edit part.
+	 * 
+	 * Body of this method does not depend on settings in generation model so
+	 * you may safely remove <i>generated</i> tag and modify it.
+	 * 
+	 * @was-generated
+	 */
+	@Override
+	protected NodeFigure createNodeFigure() {
+		NodeFigure figure = createNodePlate();
+		figure.setLayoutManager(new XYLayout());
+		IFigure shape = createNodeShape();
+		figure.add(shape);
+		contentPane = setupContentPane(shape);
+		return figure;
+	}
+
+	/**
+	 * Default implementation treats passed figure as content pane. Respects
+	 * layout one may have set for generated figure.
+	 * 
+	 * @param nodeShape
+	 *            instance of generated figure class
+	 * @was-generated
+	 */
+	protected IFigure setupContentPane(IFigure nodeShape) {
+		return nodeShape; // use nodeShape itself as contentPane
+	}
+
+	/**
+	 * @was-generated
+	 */
+	@Override
+	public IFigure getContentPane() {
+		if (contentPane != null) {
+			return contentPane;
+		}
+		return super.getContentPane();
+	}
+
+	protected Class<?> getMetamodelType() {
+		return ProcessElement.class;
+	}
+}
--- src/de/wwu/md2dot0/diagram/ProcessElementFigure.java
+++ src/de/wwu/md2dot0/diagram/ProcessElementFigure.java
@@ -1,6 +1,5 @@
 package de.wwu.md2dot0.diagram;
 
-import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.Polygon;
@@ -9,10 +8,11 @@ import org.eclipse.draw2d.XYLayout;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.sirius.diagram.CustomStyle;
+import org.eclipse.sirius.diagram.ui.tools.api.figure.AbstractTransparentRectangle;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Display;
 
-public class ProcessElementFigure extends Figure {
+public class ProcessElementFigure extends AbstractTransparentRectangle {
 
 	protected Color processElementBackgroundColor = new Color(Display.getCurrent(), 107, 218, 247);
 	protected Color processElementBorderColor = new Color(Display.getCurrent(), 0, 0, 0);
@@ -23,91 +23,97 @@ public class ProcessElementFigure extends Figure {
 	protected Polyline lineLower;
 	protected Label labelUpper;
 	protected Label labelLower;
-	protected Label labelMain;
+	// protected Label labelMain;
 
 	public ProcessElementFigure(CustomStyle style) {
 		setLayoutManager(new XYLayout());
-		
-		// Use provided styling if exists
-		if(style != null){
-			processElementBorderColor = new Color(Display.getCurrent(), style.getBorderColor().getRed(), style.getBorderColor().getGreen(), style.getBorderColor().getBlue());
-			processElementLabelColor = new Color(Display.getCurrent(), style.getLabelColor().getRed(), style.getLabelColor().getGreen(), style.getLabelColor().getBlue());
-		}
-		
+
 		arrowShape = new Polygon();
 		arrowShape.setBackgroundColor(processElementBackgroundColor);
-		arrowShape.setFill(false);
+		arrowShape.setFill(true);
 		arrowShape.setForegroundColor(processElementBorderColor);
 		arrowShape.setOutline(true);
 		add(arrowShape);
-		
+
 		lineUpper = new Polyline();
 		lineUpper.setForegroundColor(processElementBorderColor);
 		lineUpper.setLineWidth(1);
 		add(lineUpper);
-		
+
 		lineLower = new Polyline();
 		lineLower.setForegroundColor(processElementBorderColor);
 		lineLower.setLineWidth(1);
 		add(lineLower);
-		
+
 		labelUpper = new Label("Test");
 		add(labelUpper);
 
 		labelLower = new Label("TestDown");
 		add(labelLower);
-		
-		labelMain= new Label("TestMain");
-		add(labelMain);
-	}
-	
-	public void setProcessElementDataType(String type){
-		if(type == null) type = "";
-		labelLower.setText(type);
-	}
-	
-	public void setProcessElementType(String type){
-		if(type == null) type = "";
-		labelUpper.setText(type); 
-	}
-	
-	public void setProcessElementDescription(String type){
-		if(type == null) type = "";
-		labelMain.setText(type);
 	}
 
 	@Override
-	protected void paintFigure(Graphics graphics) {
+	public void paintFigure(Graphics graphics) {
+		// super.paintFigure(graphics);
 		Rectangle bounds = getBounds().getCopy();
 		int width = bounds.width;// <= 100 ? 100 : bounds.width;
 		int height = bounds.height;// <= 50 ? 50 : bounds.height;
 
-		// Calculate new dimensions
 		int[] coordsArrow = { bounds.x + 0, bounds.y + 0, 
-				bounds.x + (int) (width * 0.8), bounds.y + 0, 
-				bounds.x + width - 1, bounds.y + (int) (height / 2),
-				bounds.x + (int) (width * 0.8), bounds.y + height - 1,
-				bounds.x + 0, bounds.y + height - 1, 
+				bounds.x + (int) (width * 0.8), bounds.y + 0,
+				bounds.x + width - 1, bounds.y + (int) (height / 2), 
+				bounds.x + (int) (width * 0.8),
+				bounds.y + height - 1, bounds.x + 0, bounds.y + height - 1, 
 				bounds.x + 0, bounds.y + 0 };
 		arrowShape.setPoints(new PointList(coordsArrow));
 
 		int[] coordsLineUpper = { bounds.x + 0, bounds.y + (int) (height * 0.25), 
-				bounds.x + (int) (width * 0.9), bounds.y + (int) (height * 0.25) };
+				bounds.x + (int) (width * 0.9),
+				bounds.y + (int) (height * 0.25) };
 		lineUpper.setPoints(new PointList(coordsLineUpper));
-		
+
 		int[] coordsLineLower = { bounds.x + 0, bounds.y + (int) (height * 0.75), 
-				bounds.x + (int) (width * 0.9), bounds.y + (int) (height * 0.75) };
+				bounds.x + (int) (width * 0.9),
+				bounds.y + (int) (height * 0.75) };
 		lineLower.setPoints(new PointList(coordsLineLower));
-		
+
 		// Use current colors
 		labelUpper.setForegroundColor(processElementLabelColor);
-		labelMain.setForegroundColor(processElementLabelColor);
 		labelLower.setForegroundColor(processElementLabelColor);
-		
+
 		// Positioning
 		setConstraint(arrowShape, new Rectangle(0, 0, width, height));
 		setConstraint(labelUpper, new Rectangle(0, 0, (int) (width * 0.85), (int) (height * 0.25)));
-		setConstraint(labelMain, new Rectangle(0, (int) (height * 0.25), (int) (width * 0.85), (int) (height * 0.5)));
 		setConstraint(labelLower, new Rectangle(0, (int) (height * 0.75), (int) (width * 0.85), (int) (height * 0.25)));
 	}
+
+	public void setProcessElementDataType(String type) {
+		if (type == null)
+			type = "";
+		labelLower.setText(type);
+	}
+
+	public void setProcessElementType(String type) {
+		if (type == null)
+			type = "";
+		labelUpper.setText(type);
+	}
+
+	// setLayoutManager(new XYLayout());
+	//
+	// // Use provided styling if exists
+	// if(style != null){
+	// processElementBorderColor = new Color(Display.getCurrent(),
+	// style.getBorderColor().getRed(), style.getBorderColor().getGreen(),
+	// style.getBorderColor().getBlue());
+	// processElementLabelColor = new Color(Display.getCurrent(),
+	// style.getLabelColor().getRed(), style.getLabelColor().getGreen(),
+	// style.getLabelColor().getBlue());
+	// }
+	//
+	// public void setProcessElementDescription(String type){
+	// if(type == null) type = "";
+	// labelMain.setText(type);
+	// }
+	//
 }
