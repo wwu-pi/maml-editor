f077f2186698cbf5244378bc33210226f834044a
Christoph Rieger <christoph.rieger@wi.uni-muenster.de>
2016-04-11 17:32:27 +0200

    Prepare attribute representation

--- src/de/wwu/md2dot0/design/TestService.java
+++ src/de/wwu/md2dot0/design/TestService.java
@@ -2,6 +2,7 @@ package de.wwu.md2dot0.design;
 
 import org.eclipse.emf.ecore.EObject;
 
+import md2dot0.ParameterConnector;
 import md2dot0.ProcessElement;
 
 public class TestService {
@@ -18,5 +19,13 @@ public class TestService {
 	public String debugString(EObject elem) {
 		System.out.println(elem);
 		return "Test";
+	}
+	
+	public String getParameterConnectorLabelText(EObject obj){
+		if(obj instanceof ParameterConnector){
+			ParameterConnector connector = (ParameterConnector) obj;
+			return connector.getOrder() + ": " + (connector.getDescription().length() > 0 ? connector.getDescription() : connector.getTargetElement().getDescription());
+		}
+		return "error";
 	}
 }
