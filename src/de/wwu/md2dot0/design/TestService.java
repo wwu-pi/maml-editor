f3d3c9dacb0ed4b2234c49e8bb8787bb529860d0
Christoph Rieger <christoph.rieger@wi.uni-muenster.de>
2016-04-11 17:01:32 +0200

    Added Attribute connectors

--- src/de/wwu/md2dot0/design/TestService.java
+++ src/de/wwu/md2dot0/design/TestService.java
@@ -6,17 +6,17 @@ import md2dot0.ProcessElement;
 
 public class TestService {
 
-	    public String getProcessElementType(ProcessElement ePackage) {
-	        return "hallo";
-	    }
-	    
-	    public boolean debugTrue(EObject elem) {
-	       System.out.println(elem);
-	       return true;
-	    }
-	    
-	    public String debugString(EObject elem) {
-		       System.out.println(elem);
-		       return "Test";
-		}
+	public String getProcessElementType(ProcessElement ePackage) {
+		return "hallo";
+	}
+
+	public boolean debugTrue(EObject elem) {
+		System.out.println(elem);
+		return true;
+	}
+
+	public String debugString(EObject elem) {
+		System.out.println(elem);
+		return "Test";
+	}
 }
