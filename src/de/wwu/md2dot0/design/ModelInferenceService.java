fc59129340cea33254ccbb98b8cb97a9ca78f949
Christoph Rieger <rieger.chris@googlemail.com>
2016-07-11 16:45:17 +0200

    Vorsichtiges schrittweises clean-up

--- src/de/wwu/md2dot0/design/ModelInferenceService.java
+++ src/de/wwu/md2dot0/design/ModelInferenceService.java
@@ -2,46 +2,14 @@ package de.wwu.md2dot0.design;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
 import java.util.Optional;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.common.command.Command;
-import org.eclipse.emf.common.command.CommandStack;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.edit.command.SetCommand;
-import org.eclipse.emf.transaction.RecordingCommand;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.emf.transaction.util.TransactionUtil;
-import org.eclipse.gef.EditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
-import org.eclipse.gmf.runtime.notation.Diagram;
-import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.window.Window;
-import org.eclipse.sirius.business.api.dialect.DialectManager;
-import org.eclipse.sirius.business.api.session.Session;
-import org.eclipse.sirius.business.api.session.SessionManager;
-import org.eclipse.sirius.diagram.DDiagram;
-import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DSemanticDiagramSpec;
-import org.eclipse.sirius.diagram.ui.internal.refresh.GMFHelper;
-import org.eclipse.sirius.ui.business.api.dialect.DialectEditor;
-import org.eclipse.sirius.ui.business.api.session.IEditingSession;
-import org.eclipse.sirius.ui.business.api.session.SessionUIManager;
-import org.eclipse.sirius.viewpoint.DAnalysis;
-import org.eclipse.sirius.viewpoint.DRepresentation;
-import org.eclipse.sirius.viewpoint.DRepresentationElement;
-import org.eclipse.sirius.viewpoint.DView;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
-import org.eclipse.sirius.business.api.dialect.command.RefreshRepresentationsCommand;
-
 import de.wwu.md2dot0.inference.ModelInferrer;
 import de.wwu.md2dot0.inference.ModelInferrerManager;
 import de.wwu.md2dot0.inference.TypeLiteral;
@@ -53,38 +21,20 @@ public class ModelInferenceService {
 	// Central inference component
 	ModelInferrer inferrer;
 	
-	public String updateDataType(EObject obj, String newType){
-		System.out.println("TEST" + getDataTypeRepresentation(obj));
+	/**
+	 * Trigger inference process and visual update of elements.
+	 * @param obj
+	 */
+	public EObject updateDataType(EObject obj){
+		getDataTypeRepresentation(obj);
 		
-		System.out.println(getDataTypeRepresentation(obj));
-		
-		Session session = SessionManager.INSTANCE.getSession(obj);
-		TransactionalEditingDomain domain = session.getTransactionalEditingDomain();//TransactionUtil.getEditingDomain(obj);//
-		
+		// TODO better logic: Diff for changed elements only
 		Collection<ProcessFlowElement> pfes = ((UseCase) obj.eContainer()).getProcessFlowElements();
 		for(ProcessFlowElement pfe : pfes){
-			pfe.setChanged(!pfe.isChanged());
-			EList<EStructuralFeature> fs = pfe.eClass().getEAllStructuralFeatures();
-			Command cmd = new SetCommand(domain, pfe, fs.get(0), getDataTypeRepresentation(pfe));
-			domain.getCommandStack().execute(cmd);
+			pfe.setChanged(!pfe.isChanged()); // Artificial update that triggers redraw
 		}
-		
-//		ProcessFlowElement pfeNext = ((ProcessFlowElement) obj).getNextElements().get(0).getTargetProcessFlowElement();
-//		pfeNext.setChanged(!pfeNext.isChanged());
-//		EList<EStructuralFeature> fs = pfeNext.eClass().getEAllStructuralFeatures();
-//		Command cmd = new SetCommand(domain, pfeNext, fs.get(0), getDataTypeRepresentation(pfeNext));
-		
-		
-//		for(DView view : session.getOwnedViews()){
-//			Command cmd = new RefreshRepresentationsCommand(domain, null, view.getOwnedRepresentations());//domain, null, DialectManager.INSTANCE.getAllRepresentations(session));
-//			if (cmd.canExecute()) {
-//				domain.getCommandStack().execute(cmd);
-//			} else {
-//				System.out.println("Nope");
-//			}
-//		}
-		
-		return newType;
+
+		return obj;
 	}
 	
 	/**
@@ -99,129 +49,6 @@ public class ModelInferenceService {
 		inferrer = ModelInferrerManager.getInstance().getModelInferrer((UseCase) obj.eContainer());
 		inferrer.startInferenceProcess(useCase); // Container is the use case itself
 		
-		Session session = SessionManager.INSTANCE.getSession(obj);
-		TransactionalEditingDomain domain = session.getTransactionalEditingDomain();//TransactionUtil.getEditingDomain(obj);//
-		
-//		CommandStack stack = domain.getCommandStack();
-//		for(DView view : session.getOwnedViews()){
-//			Command cmd = new RefreshRepresentationsCommand(domain, null, view.getOwnedRepresentations());//domain, null, DialectManager.INSTANCE.getAllRepresentations(session));
-//			if (cmd.canExecute()) {
-//				domain.getCommandStack().execute(cmd);
-//			} else {
-//				System.out.println("Nope");
-//			}
-//		}
-		
-//		Collection<DRepresentation> c = DialectManager.INSTANCE.getAllRepresentations(session);
-//		
-//		CommandStack stack = domain.getCommandStack();
-//		for (DView view : session.getOwnedViews()) {
-//			for (DRepresentation rep : view.getOwnedRepresentations()) {
-//				// Find correct diagram (for the semantic element we consider)
-//				if (!(rep instanceof DSemanticDiagramSpec)
-//						|| !obj.eContainer().equals(((DSemanticDiagramSpec) rep).getTarget())) {
-//					System.out.println("skip" + rep);
-//					continue;
-//				}
-//				
-//				Command cmd = new RefreshRepresentationsCommand(domain, true, null, rep);
-//				if (cmd.canExecute()) {
-//					domain.getCommandStack().execute(cmd);
-//				} else {
-//					System.out.println("Cannot execute");
-//				}
-//			}
-//		}
-		
-//		for (IEditingSession uiSession : SessionUIManager.INSTANCE.getUISessions()) {
-//		for(DView view : session.getOwnedViews()){
-//			for(DRepresentation rep : view.getOwnedRepresentations()){
-//			System.out.println(rep);
-//			
-//		//	DiagramEditor dialectEditor = uiSession.getEditor((DDiagram) rep);
-//         //   ((DiagramEditor) dialectEditor).getDiagramGraphicalViewer().getEditPartRegistry();
-//             
-//			}
-//		 }
-//		}
-//		
-		
-		
-		
-		
-		
-//		Collection<DView> views = session.getOwnedViews();
-//		for(DView view : views){
-//			Collection<DRepresentation> c = view.getOwnedRepresentations();
-//			RecordingCommand cmd = new org.eclipse.sirius.business.api.dialect.command.RefreshRepresentationsCommand(editingDomain, true, new NullProgressMonitor());//, c);//domain, null, DialectManager.INSTANCE.getAllRepresentations(session));
-//			//if (cmd.canExecute()) {
-//			stack.execute(cmd);
-//			
-			
-//			try
-//			{
-//				editingDomain.runExclusive(new Runnable()
-//			{
-//			public void run ()
-//			{
-//			Display display = PlatformUI.getWorkbench().getDisplay();
-//			display.asyncExec(new Runnable()
-//			{
-//			public void run ()
-//			{
-//				editingDomain.getCommandStack().execute(cmd);
-//			}
-//			});
-//			}
-//			});
-//			}
-//			catch (InterruptedException e)
-//			{
-//			e.printStackTrace();
-//			}
-			
-			//} 
-//			for(DRepresentation rep : view.getOwnedRepresentations()){
-//				System.out.println(rep);
-//				
-////				for(DRepresentationElement repElem : rep.getOwnedRepresentationElements()){
-////				//	System.out.println(rep);
-////					
-////				}
-//				
-//			}
-//		}
-//		EObject root =session.getSessionResource().getContents().get(0);
-//		View dView = root.getOwnedViews().get(0);
-//		DRepresentation myRepresentation = dView.getOwnedRepresentations().get(0);
-		
-		//session.getModelAccessor();
-		//session.get
-//		for (IEditingSession uiSession : SessionUIManager.INSTANCE.getUISessions()) {
-//            DialectEditor dialectEditor = uiSession.getEditors().iterator().next();// .getEditor((DDiagram) gmfDiagram.getElement());
-//            System.out.println(dialectEditor);
-//            
-////          //  if (isEditorFor(dialectEditor, gmfDiagram)) {
-////            GraphicalEditPart part = getGraphicalEditPart(view, (DiagramEditor) dialectEditor);
-//           // }
-//        }
-		
-		// Update visual elements
-//		Session session = SessionManager.INSTANCE.getSession(obj);
-//		TransactionalEditingDomain editingDomain = session.getTransactionalEditingDomain();
-		
-		
-		
-		//stack.execute(updateCommand);
-//		EditPart.;
-		//DialectManager.INSTANCE.getRepresentations(arg0, arg1)
-		
-		//org.eclipse.sirius.diagram.ui.business.api.view.SiriusGMFHelper.
-		
-		//refresh(laneSetContainer.getParentDiagram(),
-		//		new NullProgressMonitor());
-		
-		
 		if(obj instanceof ProcessFlowElement){
 			String type = inferrer.getType((ProcessFlowElement) obj);
 			return type != null ? type : "??";
@@ -231,38 +58,6 @@ public class ModelInferenceService {
 		} 
 		return "??";
 	}
-	
-	public static GraphicalEditPart getGraphicalEditPart(View view) {
-        if (view != null) {
-            Diagram gmfDiagram = view.getDiagram();
-            // Try the active editor first (most likely case in practice)
-            IEditorPart editor = null;// EclipseUIUtil.getActiveEditor();
-//            //if (isEditorFor(editor, gmfDiagram)) {
-//             //   return getGraphicalEditPart(view, (DiagramEditor) editor);
-//            //} else
-            	if (gmfDiagram.getElement() instanceof DDiagram) {
-//                // Otherwise check all active Sirius editors
-                for (IEditingSession uiSession : SessionUIManager.INSTANCE.getUISessions()) {
-                    DialectEditor dialectEditor = uiSession.getEditor((DDiagram) gmfDiagram.getElement());
-                    if (((DiagramEditor) editor).getDiagram() == gmfDiagram) {
-                    	((DiagramEditor) editor).getDiagramGraphicalViewer().getEditPartRegistry();
-//                        return getGraphicalEditPart(view, (DiagramEditor) dialectEditor);
-//                    	final Map<?, ?> editPartRegistry = editor.getDiagramGraphicalViewer().getEditPartRegistry();
-//                        final EditPart targetEditPart = (EditPart) editPartRegistry.get(view);
-//                        if (targetEditPart instanceof GraphicalEditPart) {
-//                        	GraphicalEditPart result = (GraphicalEditPart) targetEditPart;
-//                        }
-//                        return result;
-                    }
-                }
-            }
-        }
-        return null;//Options.<GraphicalEditPart>newNone();
-    }
-    
-    private static boolean issEditorFor(IEditorPart editor, Diagram diagram) {
-        return editor instanceof DiagramEditor && ((DiagramEditor) editor).getDiagram() == diagram;
-    }
     
 	/** 
 	 * Perform the data model inference without returning a value
